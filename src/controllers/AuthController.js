c291f14c0c0677e2007c80ad16049765d41f96b3
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-06-22 22:39:18 -0300

    Implementação da funcionalidade para cadastro de usuários no DB

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,22 +1,78 @@
-const AuthValidator = require('../validators/AuthValidator');
-const { validationResult, matchedData } = require('express-validator');
+const { validationResult, matchedData } = require("express-validator");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
+const User = require("../models/User");
+const State = require("../models/State");
 
 module.exports = {
-    signIn: async (req, res) => {
-
-    },
+    signIn: async (req, res) => {},
     signUp: async (req, res) => {
         const errors = validationResult(req);
-        
-        if(!errors.isEmpty()){
-            res.json({errors: errors.mapped()});
+
+        if (!errors.isEmpty()) {
+            res.json({ errors: errors.mapped() });
             return;
         }
-        
-        const allData = matchedData(req);
-        
-        res.json({data: allData});
-        return;
-    }
-}
+
+        const data = matchedData(req);
+
+        // Verifica se o usuário com o e-mail especificado já existe no sistema
+        const user = await User.findOne({ email: data.email });
+        if (user) {
+            res.json({
+                error: {
+                    email: {
+                        msg: "Este e-mail já possui cadastro no sistema.",
+                    },
+                },
+            });
+            return;
+        }
+
+        // Verifica se o 'Estado' informado pelo usuário no momento do cadastro existe no sistema.
+        if (mongoose.isValidObjectId(data.state)) {
+            const state = await State.findById({ _id: data.state });
+            if (!state) {
+                res.json({
+                    error: {
+                        state: {
+                            msg: "O 'Estado' informado no cadastro é inválido/inexistente.",
+                        },
+                    },
+                });
+                return;
+            }
+        } else {
+            res.json({
+                error: {
+                    state: {
+                        msg: "Código de 'Estado' inválido",
+                    },
+                },
+            });
+            return;
+        }
+
+        const passwordHash = await bcrypt.hash(data.password, 10);
+        const payload = (Date.now() + Math.random()).toString();
+        const token = await bcrypt.hash(payload, 10);
+
+        const newUser = new User({
+            name: data.name,
+            email: data.email,
+            state: data.state,
+            passwordHash: passwordHash,
+            token: token,
+        });
+
+        await newUser.save((error, data) => {
+            if (error) return handleError(error);
+            res.json({
+                id: data.id,
+                token: data.token,
+            });
+            return;
+        });
+    },
+};
