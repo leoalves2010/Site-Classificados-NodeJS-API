70f8a9b6adb1da2d95ad7d798b549ffd96cd8a36
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-06-23 23:47:29 -0300

    Implementação do Controller para pegar as informações do usuário logado

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State');
+const User = require('../models/User');
+const Ad = require('../models/Ad');
+const Category = require('../models/Category');
 
 module.exports = {
     getStates: async (req, res) => {
@@ -6,7 +9,29 @@ module.exports = {
         res.json({states});
     },
     getInfo: async (req, res) => {
+        let token = req.query.token;
+        let user = await User.findOne({token});
+        let state = await State.findById(user.state);
+        let ad = await Ad.find({idUser: user._id.toString()});
 
+        let adList = [];
+
+        for(let i in ad){
+            let cat = await Category.findById(ad[i].category);
+            
+            adList.push({
+                ...ad[i],
+                category: cat.slug
+            });
+        }
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            state: state.name,
+            adList
+        });
+        return;
     },
     editAction: async (req, res) => {
         
